d36deb96521e7280af59c8eb2fd3e4182d441f65
Kay <tranlethien2002@gmail.com>
2022-11-13 16:39:31 +0700

    test

--- js/main.js
+++ js/main.js
@@ -39,20 +39,21 @@ $(document).ready(function() {
             return true;
         }
         return false;
-    } txtPass.blur(KiemTraDN);
+    }
+    txtPass.blur(KiemTraDN);
+
     function KiemTraDK() {
-        if (KiemTraSDT() == true && KiemTraPass() == true && KiemTraOTP()==true) {
+        if (KiemTraSDT() == true && KiemTraPass() == true && KiemTraOTP() == true) {
             $("#txtDK").removeAttr("disabled");
             return true;
         }
         return false;
-    } txtPass.blur(KiemTraDK);
-    $("#txtDN").click(function()
-    {
+    }
+    txtPass.blur(KiemTraDK);
+    $("#txtDN").click(function() {
         alert("Đăng nhập thành công");
     })
-    $("#txtDK").click(function()
-    {
+    $("#txtDK").click(function() {
         alert("Đăng ký thành công");
     })
     var txtOTP = $("#txtOTP");
@@ -151,177 +152,183 @@ $(document).ready(function() {
     $('#txtQuantity').change(ctrlQuantity)
 
 
-   function ctrlQuantity(e) {
-       quantity = parseInt($('#txtQuantity').val());
-       if (quantity > maxQuantity) {
-           $('#txtQuantity').val(maxQuantity);
-           tbQuantity.html('Số lượng bạn chọn đã đạt mức tối đa của sản phẩm này');
-       } else {
-           tbQuantity.html(' ');
-       }
-   }
-   // giỏ hàng
-   var i=1;
-   var txtSL=$("#txtSL");
-   var txtDG=$("#txtDG");
-   var txtTong=$("#txtTong");
-   var dongia=0;
-   var tong=0;
-
-$("#btngiam").click(function()
-{
-i= parseInt(txtSL.val());
-dongia=parseFloat(txtDG.val());
-tong=parseFloat(txtTong.val());
-if(i>=1)
-{
-  txtSL.val(i-1);
-  txtTong.val((tong-dongia)+".000");
-}
-
-});
-$("#btntang").click(function()
-{
-i=parseInt(txtSL.val());
-dongia=parseFloat(txtDG.val());
-tong=parseFloat(txtTong.val());
-txtSL.val(i+1);
-txtTong.val((tong+dongia)+".000");
-});
-$("#btn-left").click(function()
-{
-i= parseInt($("#txtSL1").val());
-dongia=parseFloat($("#txtDG1").val());
-tong=parseFloat($("#txtTong1").val());
-if(i>=1)
-{
-  $("#txtSL1").val(i-1);
-  $("#txtTong1").val((tong-dongia)+".000");
-}
-});
-$("#btn-right").click(function()
-{
-i= parseInt($("#txtSL1").val());
-dongia=parseFloat($("#txtDG1").val());
-tong=parseFloat($("#txtTong1").val());
-$("#txtSL1").val(i+1);
-$("#txtTong1").val((tong+dongia)+".000");
-})
-$("#btn-left-1").click(function(){
-   i= parseInt($("#txtSL2").val());
-dongia=parseFloat($("#txtDG2").val());
-tong=parseFloat($("#txtTong2").val());
-if(i>=1)
-{
-  $("#txtSL2").val(i-1);
-  $("#txtTong2").val((tong-dongia)+".000");
-}
-});
-$("#btn-right-1").click(function()
-{
-   i= parseInt($("#txtSL2").val());
-dongia=parseFloat($("#txtDG2").val());
-tong=parseFloat($("#txtTong2").val());
-$("#txtSL2").val(i+1);
- $("#txtTong2").val((tong+dongia)+".000");
-})
-//modal
-$("#btnModal").click(function()
-{
-    $("#myModal").modal();
-})
-function ctrlQuantity(e) {
-    quantity = parseInt($('#txtQuantity').val());
-    if (quantity > maxQuantity) {
-        $('#txtQuantity').val(maxQuantity);
-        tbQuantity.html('Số lượng bạn chọn đã đạt mức tối đa của sản phẩm này');
-    } else {
-        tbQuantity.html(' ');
+    function ctrlQuantity(e) {
+        quantity = parseInt($('#txtQuantity').val());
+        if (quantity > maxQuantity) {
+            $('#txtQuantity').val(maxQuantity);
+            tbQuantity.html('Số lượng bạn chọn đã đạt mức tối đa của sản phẩm này');
+        } else {
+            tbQuantity.html(' ');
+        }
     }
-}
+    // giỏ hàng
+    var i = 1;
+    var txtSL = $("#txtSL");
+    var txtDG = $("#txtDG");
+    var txtTong = $("#txtTong");
+    var dongia = 0;
+    var tong = 0;
 
+    $("#btngiam").click(function() {
+        i = parseInt(txtSL.val());
+        dongia = parseFloat(txtDG.val());
+        tong = parseFloat(txtTong.val());
+        if (i >= 1) {
+            txtSL.val(i - 1);
+            txtTong.val((tong - dongia) + ".000");
+        }
 
-// giỏ hàng
-var txtSL = $("tr .txtSL");
-var txtDG = $("tr .txtDG");
-var txtTong = $("tr .txtTong");
-var dongia = 0;
-var tong = 0;
-var btnGiam = $(".btngiam");
-var btnTang = $(".btntang");
-var btnXoa = $(".btn-delete")
-var sanPham = $(".cart-table tr")
+    });
+    $("#btntang").click(function() {
+        i = parseInt(txtSL.val());
+        dongia = parseFloat(txtDG.val());
+        tong = parseFloat(txtTong.val());
+        txtSL.val(i + 1);
+        txtTong.val((tong + dongia) + ".000");
+    });
+    $("#btn-left").click(function() {
+        i = parseInt($("#txtSL1").val());
+        dongia = parseFloat($("#txtDG1").val());
+        tong = parseFloat($("#txtTong1").val());
+        if (i >= 1) {
+            $("#txtSL1").val(i - 1);
+            $("#txtTong1").val((tong - dongia) + ".000");
+        }
+    });
+    $("#btn-right").click(function() {
+        i = parseInt($("#txtSL1").val());
+        dongia = parseFloat($("#txtDG1").val());
+        tong = parseFloat($("#txtTong1").val());
+        $("#txtSL1").val(i + 1);
+        $("#txtTong1").val((tong + dongia) + ".000");
+    })
+    $("#btn-left-1").click(function() {
+        i = parseInt($("#txtSL2").val());
+        dongia = parseFloat($("#txtDG2").val());
+        tong = parseFloat($("#txtTong2").val());
+        if (i >= 1) {
+            $("#txtSL2").val(i - 1);
+            $("#txtTong2").val((tong - dongia) + ".000");
+        }
+    });
+    $("#btn-right-1").click(function() {
+            i = parseInt($("#txtSL2").val());
+            dongia = parseFloat($("#txtDG2").val());
+            tong = parseFloat($("#txtTong2").val());
+            $("#txtSL2").val(i + 1);
+            $("#txtTong2").val((tong + dongia) + ".000");
+        })
+        //modal
+    $("#btnModal").click(function() {
+        $("#myModal").modal();
+    })
 
-// Tìm id của cha 
-function layIDSanPhamCha(PhanTu) {
-    return PhanTu.closest('tr').attr('id')
-}
-// Gán id khác nhau cho từng sản phẩm
-let idSP = 0;
-sanPham.each(function() {
-    $(this).attr('id', idSP);
-    ++idSP;
-})
-
-btnGiam.on('click', function() {
-    let btnId = layIDSanPhamCha($(this))
-    giam(btnId)
-})
-
-btnTang.on('click', function() {
-    let btnId = layIDSanPhamCha($(this))
-    tang(btnId)
-})
-
-btnXoa.on('click', function() {
-    let btnId = layIDSanPhamCha($(this))
-    xoaSP(btnId)
-})
-
-function giam(id) {
-    txtSL = $("tr#" + [id] + " .txtSL");
-    txtDG = $("tr#" + [id] + " .txtDG")
-    txtTong = $("tr#" + [id] + " .txtTong")
-
-    soLuongHienTai = parseInt(txtSL.val());
-    dongia = parseFloat(txtDG.html());
-    tong = parseFloat(txtTong.html());
-
-    if (soLuongHienTai > 1) {
-        txtSL.val(soLuongHienTai - 1);
-        txtTong.html((tong - dongia) + ".000");
-    } else {
-        xoaSP(id)
+    function ctrlQuantity(e) {
+        quantity = parseInt($('#txtQuantity').val());
+        if (quantity > maxQuantity) {
+            $('#txtQuantity').val(maxQuantity);
+            tbQuantity.html('Số lượng bạn chọn đã đạt mức tối đa của sản phẩm này');
+        } else {
+            tbQuantity.html(' ');
+        }
     }
-}
 
-function tang(id) {
-    txtSL = $("tr#" + [id] + " .txtSL");
-    txtDG = $("tr#" + [id] + " .txtDG")
-    txtTong = $("tr#" + [id] + " .txtTong")
-    var tongSLSanPham = txtSL.data('max')
 
-    soLuongHienTai = parseInt(txtSL.val());
-    dongia = parseFloat(txtDG.html());
-    tong = parseFloat(txtTong.html());
+    // giỏ hàng
+    var txtSL = $(".txtSL");
+    var txtDG = $(".txtDG");
+    var txtTong = $(".txtTong");
+    var dongia = 0;
+    var tong = 0;
+    var btnGiam = $(".btngiam");
+    var btnTang = $(".btntang");
+    var btnXoa = $(".btn-delete")
+    var sanPham = $(".cart-table tr")
 
-    if (soLuongHienTai < tongSLSanPham) {
-        txtSL.val(soLuongHienTai + 1);
-        txtTong.html((tong + dongia) + ".000");
+    // Tìm id của cha 
+    function layIDSanPhamCha(PhanTu) {
+        return PhanTu.closest('tr').attr('id')
     }
-}
+    // Gán id khác nhau cho từng sản phẩm
+    let idSP = 0;
+    sanPham.each(function() {
+        $(this).attr('id', idSP);
+        ++idSP;
+    })
 
-function xoaSP(id) {
-    temp = $("tr#" + [id]);
-    temp.html('')
-}
+    btnGiam.on('click', function() {
+        let btnId = layIDSanPhamCha($(this))
+        giam(btnId)
+    })
 
-// function updateTongTien() {
-//     txtTong.each(function(tong) {
-//         console.log(tong.innerHTML)
-//     })
-// }
+    btnTang.on('click', function() {
+        let btnId = layIDSanPhamCha($(this))
+        tang(btnId)
+    })
 
+    btnXoa.on('click', function() {
+        let btnId = layIDSanPhamCha($(this))
+        xoaSP(btnId)
+    })
+
+    function giam(id) {
+        txtSL = $("tr#" + [id] + " .txtSL");
+        txtDG = $("tr#" + [id] + " .txtDG")
+        txtTong = $("tr#" + [id] + " .txtTong")
+
+        soLuongHienTai = parseInt(txtSL.val());
+        dongia = parseFloat(txtDG.html());
+        tong = parseFloat(txtTong.html());
+
+        if (soLuongHienTai > 1) {
+            txtSL.val(soLuongHienTai - 1);
+            txtTong.html((tong - dongia) + ".000");
+        } else {
+            xoaSP(id)
+        }
+
+        updateTongTien()
+    }
+
+    function tang(id) {
+        txtSL = $("tr#" + [id] + " .txtSL");
+        txtDG = $("tr#" + [id] + " .txtDG")
+        txtTong = $("tr#" + [id] + " .txtTong")
+        var tongSLSanPham = txtSL.data('max')
+
+        soLuongHienTai = parseInt(txtSL.val());
+        dongia = parseFloat(txtDG.html());
+        tong = parseFloat(txtTong.html());
+
+        if (soLuongHienTai < tongSLSanPham) {
+            txtSL.val(soLuongHienTai + 1);
+            txtTong.html((tong + dongia) + ".000");
+        }
+
+        updateTongTien()
+    }
+
+    function xoaSP(id) {
+        temp = $("tr#" + [id]);
+        temp.html('')
+        updateTongTien()
+    }
+
+    function updateTongTien() {
+        var dstxtTong = $(".txtTong");
+        var tongTien = 0
+
+        for (let i = 0; i < dstxtTong.length; i++) {
+            var temp = catGT(dstxtTong[i].innerHTML)
+            var tienSP = parseInt(temp)
+            tongTien += tienSP;
+        }
+
+        $('#txtThanhTien').html(tongTien + '.000')
+    }
+
+    function catGT(chuoiGT) {
+        var temp = chuoiGT.replace('.000', '')
+        return temp
+    }
 });
-
-    
